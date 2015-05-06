c0d6b126bec082833e78de2512d948e6e3556d0c
TroyKey <tkv9795@saintjoe.edu>
2015-05-06 11:50:33 -0500

    Update date.js

--- date.js
+++ date.js
@@ -4,5 +4,6 @@ function findDate(string) {
   return new Date(Number(match[3]),
                   Number(match[1]) - 1,
                   Number(match[2]));
+                  //this change allows you to ente the date the "american way" and have it printed out
 }
 print(findDate("1-30-2003"));
